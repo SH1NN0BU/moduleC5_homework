8c5da05c0b326297cb4e9dba24db29baeed620ed
SH1NN0BU <github@nesteryuk.moscow>
2023-02-13 00:27:03 +0300

    function useRequest

--- c5_5.7.js
+++ c5_5.7.js
@@ -9,3 +9,30 @@ const page_node = document.querySelector('.input-page');
 const limit_node = document.querySelector('.input-limit');
 
 //
+let page_param = localStorage.getItem('Page_param');
+let limit_param = localStorage.getItem('Limit_param');
+
+if(page_param != null && limit_param != null)
+{
+    console.log("Use Page and Limit from local storage: ", page_param, limit_param) 
+    page_node.value = Number(page_param)
+    limit_node.value = Number(limit_param)
+    runRequest(Number(page_param), Number(limit_param))
+}
+
+function useRequest(p,l) {
+    return fetch(url_template(p, l))
+    .then((response) => {
+      // console.log("1: ", response)
+      return response.json()
+    })
+    .then((json) => {
+      // console.log("2: ", json)
+      localStorage.setItem('Page_param', p);
+      localStorage.setItem('Limit_param', l);
+      displayResult(json)
+
+    })
+    .catch(() => { console.log('fetch error') });
+};
+
